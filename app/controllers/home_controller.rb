eb687b42c71d9a70916de614ef317b13d578db4f
blaze-nitd <abhishekkarn96@gmail.com>
2017-11-13 02:33:42 +0530

    Fixed medicine not found in database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,29 @@
 class HomeController < ApplicationController
   def index
     if params[:drug].present?
-      @status = "Hola its working"
       drug = Medicine.find_by(name:params[:drug])
-      shops = Shop.all
-      total_count = 0
-      @locations = []
-      @shop_details = []
-      shops.each do |shop|
-        med_count = MedicineCount.find_by(shop_id:shop.id, medicine_id:drug.id)
-        if med_count
-          temp = [shop.latitude,shop.longitude,med_count.count]
-          @shop_details.push [shop.name,shop.address]
-          @locations.push temp
+      if drug.nil?
+        @status = nil
+      else
+        @status = "Hola its working"
+        shops = Shop.all
+        total_count = 0
+        @locations = []
+        @shop_details = []
+        shops.each do |shop|
+          med_count = MedicineCount.find_by(shop_id:shop.id, medicine_id:drug.id)
+          if med_count
+            temp = [shop.latitude,shop.longitude,med_count.count]
+            @shop_details.push [shop.name,shop.address]
+            @locations.push temp
+          end
         end
+        @longitude = params[:longitude]
+        @latitude = params[:latitude]
+        @alternative_drugs = drug.alternatives
       end
-      @longitude = params[:longitude]
-      @latitude = params[:latitude]
-      @alternative_drugs = drug.alternatives
     else
-      @status = nil
+      @status = "search"
     end
   end
-
 end
